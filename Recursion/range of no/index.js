2892cecf54a02510119129ae68fd1fa56576db86
Faiznhmd <fiaznhmd.0624@gmail.com>
2024-05-08 19:51:05 +0530

    range of no

--- Recursion/range of no/index.js
+++ Recursion/range of no/index.js
@@ -2,7 +2,7 @@ function rangeOfNO(startNum, EndNum) {
   if (EndNum < startNum) {
     return [];
   } else {
-    const numbers = rangeOfNO(startNum, EndNum - 1);
+    let numbers = rangeOfNO(startNum, EndNum - 1);
     numbers.push(EndNum);
     return numbers;
   }
